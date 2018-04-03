a7c8684295040492d494b459e5ce43c4eb20807d
Roee Shachar <roeeshachar@gmail.com>
2018-04-03 16:02:32 +0300

    refactored cassandra configurations from file

--- src/com/roee/spring/dl/cassandra/config/CassandraConfig.java
+++ src/com/roee/spring/dl/cassandra/config/CassandraConfig.java
@@ -1,30 +1,46 @@
 package com.roee.spring.dl.cassandra.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
 import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
+/**
+ *  A configuration class for cassandra connection
+ *  Turns to a properties file - cassandra.properties
+ */
 
-@EnableCassandraRepositories(basePackages = "com.roee.spring.dl.cassandra.repository")
 @Configuration
+@PropertySource(value = {"classpath:cassandra.properties"})
+@EnableCassandraRepositories(basePackages = "com.roee.spring.dl.cassandra.repository")
 public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    private final Environment environment;
+
+    @Autowired
+    public CassandraConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     protected String getKeyspaceName() {
-        return "roeekeyspace";
+        return environment.getProperty("cassandra.keyspace");
     }
+
 
     @Bean
     public CassandraClusterFactoryBean cluster() {
         CassandraClusterFactoryBean cluster =
                 new CassandraClusterFactoryBean();
-        cluster.setContactPoints("localhost");
-        cluster.setPort(9042);
+        cluster.setContactPoints(environment.getProperty("cassandra.contactpoints"));
+        cluster.setPort(Integer.parseInt(environment.getProperty("cassandra.port")));
         return cluster;
     }
 
